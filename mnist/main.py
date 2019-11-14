59ab5d77c35209e2a0ed6860089984840c2b66a4
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2019-11-14 01:12:22 -0600

    Update to train and use PFI

--- mnist/main.py
+++ mnist/main.py
@@ -1,92 +1,88 @@
+'''Train, using pytorchfi instead as a fancy dropout'''
 from __future__ import print_function
 import argparse
+import logging
+from tqdm import tqdm, trange
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import MultiStepLR
+from torchvision import datasets, transforms
+
+from pytorchfi import PyTorchFI_Util as pfi_util
+from pytorchfi import PyTorchFI_Core as pfi_core
 from resnet.models.resnet import ResNet18
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer):
+    '''Train the model'''
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    pbar = tqdm(train_loader, unit='Batches', desc='Training')
+    criterion = nn.CrossEntropyLoss()
+    # for batch_idx, (data, target) in enumerate(train_loader):
+    for data, target in pbar:
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target.to(device))
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+
+        pbar.set_postfix(loss=loss.item())
 
 
-def test(args, model, device, test_loader):
+def get_lr(mizer):
+    """Used for convenience when printing"""
+    for param_group in mizer.param_groups:
+        return param_group['lr']
+
+
+def test(model, device, test_loader):
+    '''Test the model on a validation set'''
     model.eval()
     test_loss = 0
     correct = 0
+    criterion = nn.CrossEntropyLoss()  # NOQA
+    pbar = tqdm(test_loader, unit='Batches', desc='Testing')
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in pbar:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            # sum up batch loss
+            test_loss += criterion(output, target.to(device)).item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    accuracy = 100. * correct / len(test_loader.dataset)
+    # logging.info('Validation Accuracy: %.4f Loss: %.4f', accuracy, test_loss)
+    tqdm.write(f'Validation Accuracy: {accuracy:.4f}, Loss: {test_loss:.4f}')
+    return accuracy
 
 
 def main():
+    '''Setup and iterate over training'''
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                         help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=128,
+                        metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=450, metavar='N',
                         help='number of epochs to train (default: 450)')
     parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                         help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.1, metavar='M',
-                        help='Learning rate step gamma (default: 0.1)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
+    parser.add_argument('--use-pfi', action='store_true', default=False,
+                        help='Use PFI as a dropout alternative')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
+    parser.add_argument('--log-frequency', type=int, default=50)
 
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
@@ -97,11 +93,13 @@ def main():
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
+    logging.info('Using %s', 'GPU' if use_cuda else 'CPU')
+
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10('/scratch/data', train=True, download=True,
                          transform=transforms.Compose([
-                             transforms.RandomCrop(24),
+                             transforms.RandomCrop(32, padding=4),
                              transforms.RandomHorizontalFlip(),
                              transforms.ColorJitter(brightness=0.1,
                                                     contrast=0.1,
@@ -114,7 +112,7 @@ def main():
     test_loader = torch.utils.data.DataLoader(
         datasets.CIFAR10('/scratch/data', train=True, download=True,
                          transform=transforms.Compose([
-                             transforms.RandomCrop(24),
+                             # transforms.Resize(24),
                              transforms.ToTensor(),
                              transforms.Normalize((0.4914, 0.4822, 0.4465),
                                                   (0.2023, 0.1994, 0.2010))
@@ -122,19 +120,38 @@ def main():
         batch_size=args.batch_size, shuffle=True, **kwargs)
 
     model = ResNet18().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=5e-4,
+                          momentum=0.9)
 
     scheduler = MultiStepLR(optimizer, milestones=[150, 250, 350],
-                            gamma=args.gamma)
+                            gamma=0.1)
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
-        scheduler.step()
+    if args.use_pfi:
+        mdl = model
+        name = "pfi_cifar_resnet18.pt"
+    else:
+        mdl = model
+        name = "cifar_resnet18.pt"
+
+    desc = 'PFI Training' if args.use_pfi else 'Training'
+    with trange(1, args.epochs + 1, unit='Epoch', desc=desc) as pbar:
+        acc = test(model, device, test_loader)
+        for epoch in pbar:
+            pbar.set_postfix(lr=get_lr(optimizer), acc=acc)
+            train(mdl, device, train_loader, optimizer)
+            if epoch % args.log_frequency == 0:
+                acc = test(mdl, device, test_loader)
+                if args.use_pfi and epoch == 200:
+                    pfi_core.init(model, 32, 32, 128, use_cuda=use_cuda)
+                    inj_model = pfi_util.random_inj_per_layer()
+                    mdl = inj_model
+
+            scheduler.step()
 
     if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        torch.save(inj_model.state_dict(), name)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     main()
