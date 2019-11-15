Fix sum in output and add softmax

mnist/main.py
@@ -83,7 +83,7 @@ def eval_confidences(model, device, test_loader):
             data, target = data.to(device), target.to(device)
             output = F.softmax(model(data), dim=1)
             pdb.set_trace()
-            confidences += output.cpu().numpy().sum()
+            confidences += output.sum(dim=0).cpu().numpy() / 128
             # get the index of the max log-probability
             _, pred = output.max(1)
             correct += pred.eq(target).sum().item()